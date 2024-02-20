81622a6fd492e923b07915870118d27e7c6f4909
mani sankar pasala <138246745+manisankar29@users.noreply.github.com>
2024-02-20 23:52:18 +0530

    Added code into Google_image_downloader.py file

--- Google_image_downloader.py
+++ Google_image_downloader.py
@@ -4,11 +4,9 @@ from PIL import Image
 
 def download_google_image(image_url, output_path='downloaded_image.jpg'):
     try:
-        # Download the image
         response = requests.get(image_url)
         img_data = response.content
 
-        # Save the image to the specified output path
         with open(output_path, 'wb') as img_file:
             img_file.write(img_data)
 
@@ -17,11 +15,8 @@ def download_google_image(image_url, output_path='downloaded_image.jpg'):
         print(f"Error occurred: {str(e)}")
 
 if __name__ == "__main__":
-    # Get the Google image URL from the user
     image_url = input("Enter the Google image URL: ")
 
-    # Specify the output path for the downloaded image (default is 'downloaded_image.jpg')
     output_path = input("Enter the output path for the downloaded image (default is 'downloaded_image.jpg'): ") or 'downloaded_image.jpg'
 
-    # Download the Google image
     download_google_image(image_url, output_path)
